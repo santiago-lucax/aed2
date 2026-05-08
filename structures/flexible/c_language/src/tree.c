99522882ccc704e19f1c7f09799e00423c0a0b8b
santiago_lucax <lucassantiagoforwork@gmail.com>
2026-05-07 21:14:36 -0300

    feat: more functions in tree.c

--- structures/flexible/c_language/src/tree.c
+++ structures/flexible/c_language/src/tree.c
@@ -83,6 +83,56 @@ bool is_mirror(Node *root1, Node *root2) {
     return ok;
 }
 
+// function for sums all elements in tree
+int sum_tree(Node *root) {
+    int sum = 0;
+    if (root != null) {
+        sum = root->element + sum_tree(root->left) + sum_tree(root->right);
+    }
+
+    return sum;
+}
+
+// function for count only elements pairs in tree
+int only_pairs(Node *root) {
+    int pairs = 0;
+    if (root != null) {
+        pairs = ((root->element % 2 == 0) ? 1 : 0) + only_pairs(root->left) + only_pairs(root->right);
+    }
+
+    return pairs;
+}
+
+// function for check if two tree are equals
+bool is_equals(Node *root1, Node *root2) {
+    bool ok = false;
+    if (root1 == null && root2 == null) ok = true; // both are void?
+    else if (root1 == null || root2 == null) ok = false; // only one is void?
+    else {
+        if (root1->element == root2->element) {
+            ok = is_equals(root1->left, root2->left) && is_equals(root1->right, root2->right);
+        } else ok = false;
+    }
+
+    return ok;
+}
+
+// function to check if there is a number divisible by x
+bool find_divisible(Node *root, int x) {
+    bool ok = false;
+    if (root != null) {
+        ok = (root->element % x == 0);
+        if (ok == false) {
+            ok = find_divisible(root->left, x);
+            if (ok == false) {
+                ok = find_divisible(root->right, x);
+            }
+        }
+    }
+
+    return ok;
+}
+
 // method for print tree
 void print_tree_rec(Node *root) {
     if (root != null) {
@@ -100,10 +150,26 @@ void print_tree(Tree *tree) {
 // main method for tests
 int main(int argc, char *argv[]) {
     Tree *tree = new_tree();
-    insert(tree, 4);
-    insert_two_pointers(&(tree->root), 3);
+    Tree *tree2 = new_tree();
+
+    // tree 1:
+    insert(tree, 7);
+    insert_two_pointers(&(tree->root), 12);
+    insert_two_pointers(&(tree->root), 5);
+
+    // tree 2:
+    insert(tree2, 7);
+    insert_two_pointers(&(tree2->root), 3);
+    insert_two_pointers(&(tree2->root), 5);
 
     print_tree(tree);
+    printf("---\n");
+    print_tree(tree2);
+
+    // check
+    bool equals = is_equals(tree->root, tree2->root);
+    bool exist = find_divisible(tree->root, 2);
+    printf("equals? %d\nexist: %d\n", equals, exist);
 
     return 0;
 }
